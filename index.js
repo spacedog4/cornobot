cb2ecd63a809e6128288852ede0f067419ff6dd0
André Monteiro <andre_luis_monteiro1998@hotmail.com>
2020-06-19 21:22:28 -0300

    Try catch

--- index.js
+++ index.js
@@ -3,38 +3,38 @@ const fs = require('fs');
 
 const { Client, Chat } = require('whatsapp-web.js');
 
-// Path where the session data will be stored
-const SESSION_FILE_PATH = './session.json';
-
-// Load the session data if it has been previously saved
-let sessionData;
-if(fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
-}
-
-// Use the saved values
-const client = new Client({
-    session: sessionData
-});
-
-// Save session values to the file upon successful auth
-client.on('authenticated', (session) => {
-    sessionData = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-    });
-});
-
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
-
-const brendaChatId = '554488413398@c.us';
-const groupChatId = '554498401456-1579545386@g.us'; 
-
 try {
+    // Path where the session data will be stored
+    const SESSION_FILE_PATH = './session.json';
+
+    // Load the session data if it has been previously saved
+    let sessionData;
+    if(fs.existsSync(SESSION_FILE_PATH)) {
+        sessionData = require(SESSION_FILE_PATH);
+    }
+
+    // Use the saved values
+    const client = new Client({
+        session: sessionData
+    });
+
+    // Save session values to the file upon successful auth
+    client.on('authenticated', (session) => {
+        sessionData = session;
+        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+
+    client.on('qr', qr => {
+        qrcode.generate(qr, {small: true});
+    });
+
+    const brendaChatId = '554488413398@c.us';
+    const groupChatId = '554498401456-1579545386@g.us'; 
+
     client.on('ready', async () => {
         console.log('ready');
         // const chats = await client.getChats();
@@ -65,5 +65,6 @@ try {
 
     client.initialize();
 } catch (e) {
+    console.log('Error');
     console.log(e);
 }
